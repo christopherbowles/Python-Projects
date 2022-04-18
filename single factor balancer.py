28a84b739d9ca2347b230b6b3530b0f3859ac8a9
christopherbowles <christopher.bowles@richmond.edu>
2022-04-17 21:31:54 -0400

    BuildPortfolio test function added
    updated stock class to not have end_date, added build portfolio function to make a data frame with all daily percent changes to stocks. portfolio builder will not be permanent, need to add volatility to Stock class so single factor comparisons can happen with each instance of a class

--- single factor balancer.py
+++ single factor balancer.py
@@ -3,24 +3,48 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
-
+from datetime import date
+##import requests_cache figure out how to cache calls within a class
+today = date.today()
+yf.pdr_override() ## this allows us to skip the yfinance pull and use pdr.get_data_yahoo("MSFT", start="2021-04-1", end=today.strftime("%Y-%m-%d"))
 
 
 class Stock:
-    def __init__(self, start_date, end_date, ticker):
-        self.ticker = yf.Ticker(ticker)   ##input("enter a ticker").upper() use this later to make input interactive
+    def __init__(self, start_date, ticker):
+        self.ticker = ticker.upper() ##input("enter a ticker").upper() use this later to make input interactive
         self.start_date = start_date
-        self.end_date = end_date
-        self.data = pdr.Series
-       # self.period = period ##input('enter which of the following ranges to consider 1d, 5d, 1mo, 1y, 2y, 5y') use later to make interactive
-    def EWMA(self):
-        pass
-    def SMA(self):
-
-class Portfolio:
-    pass
+        self.end_date = today.strftime("%Y-%m-%d") # self.period = period ##input('enter which of the following ranges to consider 1d, 5d, 1mo, 1y, 2y, 5y') use later to make interactive
+        self.df = pdr.get_data_yahoo(self.ticker, start = self.start_date, end = self.end_date)
+        self.df['Adj_Daily_Change'] = self.df['Adj Close'].pct_change()
+        self.df['Raw_Daily_Change'] = self.df['Close'].pct_change()
+        self.EWMA_adc = self.df['Adj_Daily_Change'].ewm(span=2, adjust = False).mean() #EWMA_adc => exponential weighted average of adjusted daily change
+        self.df['Daily EWMA'] = self.df['Adj_Daily_Change'].ewm(span=2, adjust = False).mean()
 
 
 
+MSFT = Stock("2021-05-01", 'msft')
+print(MSFT.df.shape)
+print(MSFT.df.head())
+print(MSFT.df['Adj_Daily_Change'])
+print(MSFT.EWMA_adc)
 
-Stock
+startdate = "2021-05-01"
+enddate = today.strftime("%Y-%m-%d")
+
+portfolio = pd.DataFrame()
+def BuildPortfolio():
+    SPX = Stock(startdate, '^SPX')
+
+    for i in list_of_stocks:
+        portfolio[i] = Stock(startdate, i).df['Adj_Daily_Change']
+
+
+
+list_of_stocks = ['goog', 'msft', 'ccj']
+
+BuildPortfolio()
+print(portfolio.shape)
+print(portfolio.head())
+
+
+
